17b0b1359baa78c569f6fe7b498cdd53d459821e
Prasham1710 <jainprasham17@gmail.com>
2022-11-13 03:26:31 +0530

    logo change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../assets/logo.png'
+
 
 const Navbar = () => {
 return (
-    <div className = 'fixed w-full h-[80px] flex justify-between items-center px-4 bg-black '>
+    <div className = 'fixed w-full h-[80px] flex justify-between items-center px-4 bg-black text-gray-300 '>
+        <div>
+            <img src ={Logo} alt="Logo Image" style={{width: '50px'}}/>
+        </div>
 
     </div>
 )
