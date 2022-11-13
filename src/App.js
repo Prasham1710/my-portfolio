48a9aa92b1dc04f23b9960b88826084f24b7eb0e
Prasham1710 <jainprasham17@gmail.com>
2022-11-13 08:40:41 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
@@ -10,6 +11,7 @@ function App() {
       <Home/>
       <About/>
       <Skills/>
+      <Contact/>
     </div>
   );
 }
